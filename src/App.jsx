038ae38d403d574c4e3cde06d06a7d3091e3a531
Cesare10-dev <cesare1087@gmail.com>
2023-07-14 22:53:19 +0200

    buttons

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,25 @@ function App() {
         <h1 className="text-center font-bold text-2xl">
           Answer to this question with a number
         </h1>
-        <ul className="flex">
+        <ul className="flex justify-center gap-1">
           {array.map((item) => (
             <li>
-              <button className="border rounded w-8 h-8 hover:bg-red-400">
+              <button className="border rounded w-8 h-8 hover:bg-red-300 hover:text-white">
                 {item}
               </button>
             </li>
           ))}
         </ul>
+
+        <div className="flex justify-between">
+          <div>Not likely</div>
+          <div>Super likely</div>
+        </div>
+
+        <div className="flex justify-between">
+          <button>Skip</button>
+          <button className="bg-purple-400 p-3 rounded">Submit</button>
+        </div>
       </div>
     </div>
   );
